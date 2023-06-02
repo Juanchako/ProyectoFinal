4196577909f0d150c70142fe104c9fc95d600a8f
Dasomacha <110133166+Dasomacha@users.noreply.github.com>
2023-06-02 15:34:39 -0500

    new

---
+++ tarea4/js/script.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal();
+  sr.reveal("header", {
+    duration: 1000,
+    origin: "top",
+    distance: '100px'
+  });
+  sr.reveal(".texto", {
+    duration: 3000,
+  });
+  sr.reveal(".diana1,.maira1,.danna1,.sebas1", {
+    duration: 2000,
+    origin: "left",
+    distance: '100px'
+  });
