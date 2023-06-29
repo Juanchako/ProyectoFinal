ae71247b172c304daf97d65ebda104e0280fe191
Dasomacha <110133166+Dasomacha@users.noreply.github.com>
2023-06-29 08:11:25 -0500

    new

--- tarea4/js/script.js
+++ tarea4/js/script.js
@@ -1,14 +1,65 @@
-window.sr = ScrollReveal();
-  sr.reveal("header", {
+var elementos = document.querySelectorAll('.diana1,.maira1,.danna1,.sebas1');
+var header = document.querySelector('header');
+
+function animarElementos() {
+  var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+  elementos.forEach(function(elemento) {
+    var rect = elemento.getBoundingClientRect();
+    var elementoTop = rect.top + scrollY;
+
+    var isVisible = (
+      elementoTop >= scrollY &&
+      elementoTop <= (scrollY + window.innerHeight)
+    );
+
+    if (isVisible) {
+      if (!elemento.classList.contains('animado')) {
+        elemento.style.opacity = '0';
+        elemento.style.transform = 'translateY(20px)';
+
+        elemento.animate([
+          { opacity: '0', transform: 'translateX(-40px)' },
+          { opacity: '1', transform: 'translateY(0)' }
+        ], {
+          duration: 1000,
+          easing: 'ease-out',
+          fill: 'forwards'
+        });
+
+        elemento.classList.add('animado');
+      }
+    } else {
+      elemento.classList.remove('animado');
+    }
+  });
+}
+function animarHeader() {
+  var rect = header.getBoundingClientRect();
+  var isVisible = (
+  rect.top >= 0 &&
+  rect.left >= 0 &&
+  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+  
+  if (isVisible && !header.classList.contains('animado')) {
+    header.style.transform = 'translateX(-100%)';  
+    header.animate([
+        { transform: 'translateY(-100%)' },
+        { transform: 'translateX(0)' }
+      ], {
     duration: 1000,
-    origin: "top",
-    distance: '100px'
-  });
-  sr.reveal(".texto", {
-    duration: 3000,
-  });
-  sr.reveal(".diana1,.maira1,.danna1,.sebas1", {
-    duration: 2000,
-    origin: "left",
-    distance: '100px'
-  });
+    easing: 'ease-out',
+    fill: 'forwards'
+    });
+    header.classList.add('animado');
+  }
+}
+  
+window.addEventListener('scroll', animarElementos);
+  
+// Llamar a la función una vez al cargar la página
+animarElementos();
+animarHeader();
+  
